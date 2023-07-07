d6cf9245bee5a08b6482c0075f0dc34813701487
Ahsan Ullah <125121855+ahsanullah111@users.noreply.github.com>
2023-07-07 17:22:19 +0500

    React-Expense-Tracker

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from 'react';
+
+// Import the Global State
+import { GlobalContext } from '../context/GlobalState';
+
+
+export const AddTransaction = () => {
+
+    const [description, setDescription] = useState('');
+    const [transactionAmount, setTransactionAmount] = useState('');
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+      
+        const newTransaction = {
+          id: new Date().getTime(),
+          description,
+          transactionAmount: +transactionAmount
+        };
+      
+        addTransaction(newTransaction);
+      
+        // Reset input fields
+        setDescription('');
+        setTransactionAmount('');
+      };
+
+    return (
+        <div>
+            <h3>Add New Transaction</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-control">
+                    <label htmlFor="description">
+                        Description
+                    </label>
+                    <input  type="text" 
+                            id="description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Detail of Transaction" 
+                            required="required"
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="transactionamount">
+                        Transaction Amount
+                    </label>
+                    <input  type="number" 
+                            id="transactionamount"
+                            value={transactionAmount}
+                            onChange={(e) => setTransactionAmount(e.target.value)}
+                            placeholder="Amount"
+                            required="required"
+                    />
+                </div>
+                <button className="btn">Add Transaction</button>
+            </form>
+        </div>
+    )
+}
